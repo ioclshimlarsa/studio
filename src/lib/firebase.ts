c64113dee9c4edf15ca6eb2c21263f3cedf39ce7
SRMP 2023 Shimla RSA <srmpshimlarsa@gmail.com>
2025-08-23 11:39:48 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,8 @@
 
 import admin from 'firebase-admin';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const projectId = process.env.FIREBASE_PROJECT_ID;
 const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
